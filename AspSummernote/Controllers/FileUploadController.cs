8156a5af916d6950b6a4b97ca5c141d5a0cc2c83
Khode Me <phearakroun@gmail.com>
2022-10-23 20:02:56 +0700

    Asp Summernote

---
+++ AspSummernote/Controllers/FileUploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace AspSummernote.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        [HttpPost]
+        [Route("api/upload")]
+        [System.Web.Mvc.HandleError]
+        public IHttpActionResult Post()
+        {
+            var files = HttpContext.Current.Request.Files["uploadFiles"];
+
+            string fileName = Path.GetFileNameWithoutExtension(files.FileName);
+            string extension = Path.GetExtension(files.FileName);
+
+            string newFileName = fileName + "-" + DateTime.Now.ToString("yyyymmssffff") + extension;
+            string UploadPath = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/Image/"), newFileName);
+            var newpaht = "/Content/Image/" + newFileName;
+
+            files.SaveAs(UploadPath);
+            return Ok(newpaht);
+        }
+    }
+}
